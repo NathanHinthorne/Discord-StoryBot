5ca19b78b50523d81ceb817874e1ecacdb04e179
Nathan Hinthorne <nathanrh856@gmail.com>
2025-05-04 21:06:52 -0700

    ADD: dev script to minify json

---
+++ dev_scripts/minify_json.py
@@ -0,0 +1,12 @@
+import json
+
+INPUT_FILE_PATH = "firebase_credentials.json"
+
+with open(INPUT_FILE_PATH) as f:
+    cred = json.load(f)
+    output = json.dumps(cred)
+    # write to output file
+    with open(f"{INPUT_FILE_PATH}_minified.json", "w") as f:
+        f.write(output)
+
+
